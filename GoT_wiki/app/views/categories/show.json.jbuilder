b825f01d2a68d802f16792a0cff1ba15e1b787b7
Christopher Smith <smithopher87@gmail.com>
2015-09-17 11:41:19 -0400

    created category model, migrated to db

---
+++ GoT_wiki/app/views/categories/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @category, :id, :type_name, :description, :created_at, :updated_at
--- GoT_wiki/db/schema.rb
+++ GoT_wiki/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917153122) do
+ActiveRecord::Schema.define(version: 20150917154009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150917153122) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "type_name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
